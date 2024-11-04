eb08e644eda9e7e678a6f7dc8be73d88e8408d82
John Rothman <johns.roth@gmail.com>
2024-11-04 16:18:50 +0100

    Update churn_library.py
    should fix issues for people running in specific enviornments

--- churn_library.py
+++ churn_library.py
@@ -9,6 +9,8 @@ import os
 import joblib
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split, GridSearchCV
